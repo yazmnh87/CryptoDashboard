a0442a8123479981d8ad9bc8b3de4fedb7f6c811
William Violette <violettewj@gmail.com>
2019-03-19 09:46:23 -0600

    added get request

--- client/src/context.js
+++ client/src/context.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 const Context = React.createContext();
+
    
 
 export class Provider extends Component {
     state = {
 
+    }
+
+
+    componentDidMount(){
+      axios.get(`https://vschool-cors.herokuapp.com?url=https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR&api_key=${process.env.REACT_APP_MM}`)
+      .then(res => console.log(res.data))
     }
 
   render() {
