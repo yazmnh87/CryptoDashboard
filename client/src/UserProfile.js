ff948a3c9f1461e24db966223e75702b9e634caf
Stuart Gardner <stuarttgardner@gmail.com>
2019-03-19 11:59:25 -0600

    changed user profile

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from "react-router-dom"
 
-const UserProfile = () => {
+class UserProfile extends Component {
+    constructor() {
+        super()
+        this.state = {
+            logo: []
+        }
+    }
+    render() {
     return (
         <div className="UserProfile" style={{borderRight:"solid white", }}>
             <img style={{width:150, display:'block',margin:"auto", marginTop:15}} src="https://png.pngtree.com/svg/20170602/b7c3ca6e9e.png" alt=""/>
             <h1 style={{textAlign:"center"}}>Name</h1>
             <hr style={{marginTop:"-10px", marginBottom:"-10px"}}/>
             <h1 style={{textAlign:"center"}}>Coin Name</h1>
-            <img style={{width:150, display:'block',margin:"auto", marginTop:30}} src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png" alt=""/>
+            <img style={{width:150, display:'block',margin:"auto", marginTop:30}} src={`${logo}`} alt=""/>
             <h1 style={{textAlign:"center", color:"red"}}>Price</h1>
             <h1 style={{textAlign:"center"}}>Percentage</h1>
             <select style={{display:"block", margin:"auto", zoom:"200%"}}>
-                <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>Coin Name</option>
-                <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>Coin Name</option>
-                <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>Coin Name</option>
-                <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>Coin Name</option>
+                <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>Bitcoin</option>
+                <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>Ethereum</option>
+                <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>XRP</option>
+                <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>Litecoin</option>
+                <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>EOS</option>
             </select>
         </div>
     );
+    }
 };
 
 export default UserProfile;
