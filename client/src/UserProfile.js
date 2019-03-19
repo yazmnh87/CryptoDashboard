c0e5586e3a833ce64afab9c4f6d4de7f4efe6e16
Stuart Gardner <stuarttgardner@gmail.com>
2019-03-19 14:48:07 -0600

    random

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -8,19 +8,21 @@ class UserProfile extends Component {
             logo: []
         }
     }
-    render() {
+    
+   render() {
     return (
         <Consumer>
             {value =>{
-                console.log(value.coins.DISPLAY)
+                // console.log(value.coins.DISPLAY)
             return (
+                
          <div className="UserProfile" style={{borderRight:"solid white", }}>
             <img style={{width:150, display:'block',margin:"auto", marginTop:15}} src="https://png.pngtree.com/svg/20170602/b7c3ca6e9e.png" alt=""/>
             <h1 style={{textAlign:"center"}}>Name</h1>
             <hr style={{marginTop:"-10px", marginBottom:"-10px"}}/>
-            <h1 style={{textAlign:"center"}}>Coin Name</h1>
-            <img style={{width:150, display:'block',margin:"auto", marginTop:30}} src={`${this.state.logo}`} alt=""/>
-            <h1 style={{textAlign:"center", color:"red"}}>{value.coins.DISPLAY ? value.coins.DISPLAY.BTC.USD.PRICE : null}</h1>
+            <h1 style={{textAlign:"center"}}>COIN NAME</h1>
+            <img style={{width:150, display:'block',margin:"auto", marginTop:30}} alt=""/>
+            <h1 style={{textAlign:"center", color:"red"}}>{value.coins.DISPLAY ? value.coins.DISPLAY.BTC.USD.PRICE : null} </h1>
             <h1 style={{textAlign:"center"}}>Percentage</h1>
             <select style={{display:"block", margin:"auto", zoom:"200%"}}>
                 <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>Bitcoin</option>
@@ -30,11 +32,12 @@ class UserProfile extends Component {
                 <option style={{marginTop:"1%", marginBottom:"1%", color:"#5ae0e7", textDecoration:"none"}}>EOS</option>
             </select>
         </div>
+                
             )
         }}
         </Consumer>
     );
     }
-};
+}
 
 export default UserProfile;
