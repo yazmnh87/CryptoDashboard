7fc130ff9584240b547916e0e9dc0dfd72fe62f4
Stuart Gardner <stuarttgardner@gmail.com>
2019-03-20 10:43:57 -0600

    added get historical data

--- client/src/context.js
+++ client/src/context.js
@@ -7,15 +7,70 @@ const Context = React.createContext();
 export class Provider extends Component {
     state = {
       coins: [],
-      coinNames: []
+      coinNames: [],
+      currentCoin: "BTC",
+      BTCHISTORICAL: [],
+      ETHHISTORICAL: [],
+      XRPHISTORICAL: [],
+      LTCHISTORICAL: [],
+      EOSHISTORICAL: []
     }
 
     componentDidMount(){
       this.getCoinData() 
       this.getCoinData2()
+      this.getHistoricalBTC()
+      this.getHistoricalETH()
+      this.getHistoricalXRP()
+      this.getHistoricalLTC()
+      this.getHistoricalEOS()
  }
-
-
+    getHistoricalBTC = () => {
+      axios.get(`https://vschool-cors.herokuapp.com?url=https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=30&aggregate=1&e=CCCAGG&api_key=${process.env.REACT_APP_NN}`)
+      .then(res=> {
+        this.setState({
+          BTCHISTORICAL: res.data
+        })
+        console.log(this.state.BTCHISTORICAL)
+      })
+    }
+    
+      getHistoricalETH = () => {
+      axios.get(`https://vschool-cors.herokuapp.com?url=https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&limit=30&aggregate=1&e=CCCAGG&api_key=${process.env.REACT_APP_NN}`)
+      .then(res=> {
+        this.setState({
+          ETHHISTORICAL: res.data
+        })
+        console.log(this.state.ETHHISTORICAL)
+      })
+    }
+      getHistoricalXRP = () => {
+      axios.get(`https://vschool-cors.herokuapp.com?url=https://min-api.cryptocompare.com/data/histoday?fsym=XRP&tsym=USD&limit=30&aggregate=1&e=CCCAGG&api_key=${process.env.REACT_APP_NN}`)
+      .then(res=> {
+        this.setState({
+          XRPHISTORICAL: res.data
+        })
+        console.log(this.state.XRPHISTORICAL)
+      })
+    }
+      getHistoricalLTC = () => {
+      axios.get(`https://vschool-cors.herokuapp.com?url=https://min-api.cryptocompare.com/data/histoday?fsym=LTC&tsym=USD&limit=30&aggregate=1&e=CCCAGG&api_key=${process.env.REACT_APP_NN}`)
+      .then(res=> {
+        this.setState({
+          LTCHISTORICAL: res.data
+        })
+        console.log(this.state.LTCHISTORICAL)
+      })
+    }
+      getHistoricalEOS = () => {
+      axios.get(`https://vschool-cors.herokuapp.com?url=https://min-api.cryptocompare.com/data/histoday?fsym=EOS&tsym=USD&limit=30&aggregate=1&e=CCCAGG&api_key=${process.env.REACT_APP_NN}`)
+      .then(res=> {
+        this.setState({
+          EOSHISTORICAL: res.data
+        })
+        console.log(this.state.EOSHISTORICAL)
+      })
+    }
     getCoinData = () => {
       axios.get(`https://vschool-cors.herokuapp.com?url=https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH,EOS,XRP,BTC,LTC&tsyms=BTC,USD,EUR&api_key=${process.env.REACT_APP_MM}`)
       .then(res => {
@@ -33,7 +88,9 @@ export class Provider extends Component {
         this.setState({
           coinNames: data.slice(0, 15)
         })
+        console.log(this.state.coinNames)
       })
+      
     }
 
   render() {
